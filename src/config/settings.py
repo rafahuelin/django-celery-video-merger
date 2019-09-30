5ac9527e8fe754aa8d5c2db869287d1a111039fb
Rafa <rafahuelin@gmail.com>
2019-09-30 17:03:54 +0200

    create video_merger app

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom Apps
+    'video_merger.apps.VideoMergerConfig',
 ]
 
 MIDDLEWARE = [
@@ -98,3 +101,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
---
+++ src/video_merger/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VideoMergerConfig(AppConfig):
+    name = 'video_merger'
